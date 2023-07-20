2b40e39d59849486f3c396e0ac2c350c47d2de8f
Cavdy <cavdyofficials@gmail.com>
2023-07-20 20:42:16 +0100

    setup tailwind shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Router from './router/router';
+import './index.css';
 
 const queryClient = new QueryClient();
 
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react-swc';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), splitVendorChunkPlugin()],
+  plugins: [react(), splitVendorChunkPlugin()],
   build: {
     outDir: path.join(__dirname, '..', 'server', 'admin'),
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 });
