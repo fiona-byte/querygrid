e771e74b41935c37331d9603fc3175fc6596bc3d
Cavdy <cavdyofficials@gmail.com>
2023-09-17 17:24:40 +0100

    switch editor

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,6 +7,7 @@ import '@fontsource/saira/600.css';
 import '@fontsource/ibm-plex-sans/400.css';
 import '@fontsource/ibm-plex-sans/500.css';
 import './i18n/index.ts';
+import './editorWorker.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
